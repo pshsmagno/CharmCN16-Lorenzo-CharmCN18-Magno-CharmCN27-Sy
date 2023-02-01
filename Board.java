3af60693c1c83d80cc741221fa29b51a217b03fd
pshsmagno <97730523+pshsmagno@users.noreply.github.com>
2023-02-01 22:46:43 +0800

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,24 @@
+public class Board {
+  
+  public void ShiftLeft() throws InvalidMoveException{
+    
+  }
+
+  public void ShiftRight() throws InvalidMoveException{
+    
+  }
+
+  public void ShiftTop() throws InvalidMoveException{
+    
+  }
+
+  public void ShiftDown() throws InvalidMoveException{
+    
+  }
+
+  public void addEnergy(){
+    
+  }
+
+  
+}
