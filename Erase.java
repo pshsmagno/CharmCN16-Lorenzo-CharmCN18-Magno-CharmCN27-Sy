980ec6b401573df5a63346b3623d3af11a5bb64b
pshsmagno <97730523+pshsmagno@users.noreply.github.com>
2023-02-01 22:49:26 +0800

    Create Erase.java

---
+++ Erase.java
@@ -0,0 +1,8 @@
+public class Erase extends PowerUp{
+  public Erase(String n, int c){
+    super(n, c);
+  }
+ public void activateErase() throws InsufficientEnergyException, InvalidEraseException{
+   
+ }
+}
